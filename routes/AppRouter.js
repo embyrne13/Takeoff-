03d0b40b895778959088af260f5d3160d5df7785
Emily Byrne <embyrne13@gmail.com>
2022-09-07 09:53:43 -0400

    update app router

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,6 +1,10 @@
 const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
+const TicketRouter = require('./TicketRouter')
+const FlightRouter = require('./FlightRouter')
 
 Router.use('/user', UserRouter)
+Router.use('/ticket', TicketRouter)
+Router.use('/flight', FlightRouter)
 
 module.exports = Router
